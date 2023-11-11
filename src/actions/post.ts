7a533c2f38ced3c55b6ceb06c73906e5ca8e3593
nakamoto <zksytmkn4869@gmail.com>
2023-11-11 13:34:28 +0900

    ✨

---
+++ src/actions/post.ts
@@ -0,0 +1,229 @@
+"use server";
+
+import { authGuard } from "@/actions/auth";
+import { db, deleteImage, putImage } from "@/actions/lib";
+import { Prisma } from "@prisma/client";
+import { randomUUID } from "crypto";
+import { th } from "date-fns/locale";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { cache } from "react";
+import { z } from "zod";
+
+const PostSchema = z.object({
+  body: z.string().max(140),
+});
+
+export const createPost = async (formData: FormData) => {
+  const authorId = authGuard();
+  const id = randomUUID();
+  const validatedData = PostSchema.parse({
+    body: formData.get("body"),
+  });
+  const newData: Prisma.PostUncheckedCreateInput = {
+    ...validatedData,
+    id,
+    authorId,
+  };
+
+  const thumbnailDataURL = formData.get("thumbnail") as string;
+
+  if (thumbnailDataURL) {
+    newData.thumbnailURL = await putImage(thumbnailDataURL, `posts/${id}/thumbnail.png`);
+  }
+
+  await db.post.create({
+    data: newData,
+  });
+
+  revalidatePath("/");
+  redirect("/");
+};
+
+export const updatePost = async (id: string, formData: FormData) => {
+  const authorId = authGuard();
+  const validatedData = PostSchema.parse({
+    body: formData.get("body"),
+  });
+  const newData: Prisma.PostUncheckedUpdateInput = {
+    body: validatedData.body,
+  };
+
+  const thumbnailDataURL = formData.get("thumbnail") as string;
+  const thumbnailAction = formData.get("thumbnail-action") as string;
+  const imagePath = `posts/${id}/thumbnail.png`;
+
+  if (thumbnailDataURL && thumbnailAction === "save") {
+    newData.thumbnailURL = await putImage(thumbnailDataURL, imagePath);
+  } else if (thumbnailAction === "delete") {
+    newData.thumbnailURL = null;
+    await deleteImage(imagePath);
+  }
+
+  await db.post.update({
+    where: {
+      id,
+      authorId,
+    },
+    data: newData,
+  });
+
+  revalidatePath("/");
+};
+
+export const deletePost = async (id: string, imageURL?: string | null) => {
+  const uid = authGuard();
+
+  await db.post.delete({
+    where: {
+      id,
+      authorId: uid,
+    },
+  });
+
+  if (imageURL) {
+    deleteImage(imageURL.replace(process.env.IMAGE_HOST_URL as string, ""));
+  }
+
+  revalidatePath("/");
+  redirect("/");
+};
+
+export const getPost = cache(async (id: string) => {
+  return db.post.findFirst({
+    where: {
+      id,
+    },
+  });
+});
+
+export const getOwnPost = async (id: string) => {
+  const authorId = authGuard();
+
+  return db.post.findFirst({
+    where: {
+      id,
+      authorId,
+    },
+  });
+};
+
+export const getPosts = cache(async () => {
+  return db.post.findMany({
+    take: 10,
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      author: true,
+    },
+  });
+});
+
+export const getPostCount = cache(async () => {
+  return db.post.count();
+});
+
+export const hasLike = cache(async (id: string) => {
+  const uid = authGuard();
+  const post = await db.post.findFirst({
+    where: {
+      id,
+      likes: {
+        some: {
+          id: uid,
+        },
+      },
+    },
+  });
+
+  return !!post;
+});
+
+export const toggleLike = async (id: string) => {
+  const uid = authGuard();
+  const hasLike = await db.post.findFirst({
+    where: {
+      id,
+      likes: {
+        some: {
+          id: uid,
+        },
+      },
+    },
+  });
+
+  if (hasLike) {
+    await db.post.update({
+      where: {
+        id,
+      },
+      data: {
+        likes: {
+          disconnect: {
+            id: uid,
+          },
+        },
+      },
+    });
+  } else {
+    await db.post.update({
+      where: {
+        id,
+      },
+      data: {
+        likes: {
+          connect: {
+            id: uid,
+          },
+        },
+      },
+    });
+  }
+
+  revalidatePath("/");
+};
+
+const getUserPosts = cache(async (id: string) => {
+  return db.post.findMany({
+    where: {
+      authorId: id,
+    },
+    take: 10,
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      author: true,
+    },
+  });
+});
+
+export const getMyPosts = cache(async () => {
+  const uid = authGuard();
+  const posts = await getUserPosts(uid);
+
+  return posts;
+});
+
+export const getMyLikes = cache(async () => {
+  const id = authGuard();
+  const user = await db.user.findFirst({
+    where: {
+      id,
+    },
+    include: {
+      likes: {
+        include: {
+          author: true,
+        },
+      },
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user.likes;
+});
---
+++ src/components/post-card.tsx
@@ -0,0 +1,55 @@
+import { hasLike } from "@/actions/post";
+import LikeButton from "@/components/like-button";
+import RelativeTimestamp from "@/components/relative-timestamp";
+import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs";
+import { Prisma } from "@prisma/client";
+import { Pen, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+type PostWithOwner = Prisma.PostGetPayload<{
+  include: { author: true };
+}>;
+
+export default async function PostCard({ post }: { post: PostWithOwner }) {
+  const { userId } = auth();
+
+  return (
+    <div className="py-4">
+      {post.thumbnailURL && (
+        <div className="aspect-video relative w-full overflow-hidden rounded-lg mb-4">
+          <Image sizes="800px" src={post.thumbnailURL} className="object-cover" fill alt="" />
+        </div>
+      )}
+      <div className="flex items-center mb-2">
+        <div className="w-12 h-12 rounded-full grid place-content-center overflow-hidden bg-gray-100 mr-3 relative">
+          {post.author.profileImageURL ? (
+            <Image sizes="48px" src={post.author.profileImageURL} className="object-cover" fill alt="" />
+          ) : (
+            <User size={24} className="text-gray-400" />
+          )}
+        </div>
+        <div className="flex-1">
+          <p className="mb-2 leading-none text-gray-700 font-semibold">{post.author.name}</p>
+          <p className="text-muted-foreground text-gray-700 leading-none text-sm">
+            <RelativeTimestamp date={post.createdAt} />
+          </p>
+        </div>
+      </div>
+
+      <p>{post.body}</p>
+
+      <div className="flex gap-2 text-gray-500 justify-end mt-2">
+        {userId && <LikeButton id={post.id} hasLike={await hasLike(post.id)} />}
+        {userId === post.authorId && (
+          <Button size="icon" variant="ghost" className="rounded-full" asChild>
+            <Link href={`/posts/${post.id}/edit`}>
+              <Pen size={20} />
+            </Link>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
