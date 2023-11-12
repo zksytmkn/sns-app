fa4bddae4291e0dfe2245f783bb070fc87a69ec6
nakamoto <zksytmkn4869@gmail.com>
2023-11-12 18:04:53 +0900

    🐛

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,27 +1,22 @@
 import { AppConfig } from "@/app.config";
-import { currentUser } from "@/actions/user";
 import UserMenu from "@/components/header/user-menu";
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default async function Header() {
-  const user = await currentUser();
-
+export default function Header() {
   return (
     <header className="flex gap-2 items-center h-14 px-6">
       <Link href="/" className="font-black text-xl">
         {AppConfig.title}
       </Link>
       <span className="flex-1"></span>
-      {user && (
-        <>
-          <Button size="sm" asChild>
-            <Link href="/create">ポストする</Link>
-          </Button>
-          <UserMenu />
-        </>
-      )}
+      <SignedIn>
+        <Button size="sm" asChild>
+          <Link href="/create">ポストする</Link>
+        </Button>
+        <UserMenu />
+      </SignedIn>
       <SignedOut>
         <SignInButton>
           <Button variant="outline">ログイン</Button>
